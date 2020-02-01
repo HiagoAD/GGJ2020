0b32a4cfbe3b29b59396eff4a328d5b4fe69942c
João Vitor Albuquerque <jv.albuquerque@live.com>
2020-02-01 16:33:30 -0300

    Create spawner and the mockup of enemy controller

---
+++ GGJ2020/Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header ("Status")]
+    [SerializeField] float moveSpeed = 1;
+    private bool alive;
+    private SpawnController spawner;    
+    private int ID; //Verify what type this enemy is
+
+    public bool Alive {get => alive;}
+
+    public void Init(SpawnController spawner, int ID)
+    {
+        this.spawner = spawner;
+        this.ID = ID;
+        alive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Kill();
+
+        if(alive)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(20,0,0), moveSpeed*Time.deltaTime);
+        }
+    }
+
+    public void Restart()
+    {
+        transform.position = spawner.transform.position;
+        alive = true;
+    }
+
+    private void Kill()
+    {
+        alive = false;
+        transform.position = new Vector2 (1000, 1000);
+    }
+}
---
+++ GGJ2020/Assets/Scripts/Enemies/SpawnController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    [System.Serializable]
+    struct Enemy
+    {
+        public GameObject main;
+        [HideInInspector] public List<GameObject> buffer;
+    }
+    [SerializeField] Enemy[] enemies;
+    
+    private void Awake()
+    {
+        //initialize all the list of enemies
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].buffer = new List<GameObject>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+            Spawn();
+    }
+
+    private void Spawn()
+    {
+        int enemyType = Random.Range(0, enemies.Length);
+
+        GameObject spawned = TryGetEnemy(enemyType);
+        enemies[enemyType].buffer.Add(spawned);
+    }
+
+    private GameObject TryGetEnemy(int id)
+    {
+        foreach (var enemy in enemies[id].buffer)
+        {
+            var controller = enemy.GetComponent<EnemyController>();
+            if(!controller.Alive)
+            {
+                controller.Restart();
+                return enemy;
+            }
+        }
+
+        GameObject spawned = Instantiate(enemies[id].main, transform.position, Quaternion.identity);
+
+        spawned.GetComponent<EnemyController>().Init(this, id);
+
+        return spawned;
+    }
+
+
+}
