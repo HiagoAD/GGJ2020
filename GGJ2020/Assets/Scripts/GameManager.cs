414abe9c1cea43af4dfad4eeceffe43cb82579d1
Hiago Almeida Dias <h_a_d@hotmail.com.br>
2020-02-01 15:51:53 -0300

    - Basic spawn every X seconds - Enemies going straight foward to the player

---
+++ GGJ2020/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static public GameManager Instance
+    {
+        get; private set;
+    }
+
+    [SerializeField]
+    Player player = null;
+
+    private void Start()
+    {
+        if(Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public Player GetPlayer()
+    {
+        return player;
+    }
+}
